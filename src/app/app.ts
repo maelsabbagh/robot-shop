ef75b798651291f2480db9ef4c1be4961de4e265
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-07-11 18:12:25 +0300

    adding site header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./home/home";
 import { Catalog } from "./catalog/catalog";
+import { SiteHeader } from "./site-header/site-header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Catalog],
+  imports: [RouterOutlet, Home, Catalog, SiteHeader],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
