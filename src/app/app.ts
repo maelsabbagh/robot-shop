4e23424fc36752db79fc781433ae00eec40521aa
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-07-11 18:05:57 +0300

    adding catalog component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./home/home";
+import { Catalog } from "./catalog/catalog";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Catalog],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
