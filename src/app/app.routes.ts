a8442713dcc8af4b4246b92e0e26332f1db12116
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-07-17 12:41:11 +0300

    redirect routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { Home } from './home/home';
 import { Cart } from './cart/cart';
 import { Catalog } from './catalog/catalog';
 
+// order matters, angular start matching urls with the order of routes you have provided
 export const routes: Routes = [
     {path:'home',component:Home,title:'Home - Robot Shop'},
     {path:'catalog',component:Catalog},
-    {path:'cart',component:Cart}
+    {path:'cart',component:Cart},
+    {path:'',redirectTo:'/home',pathMatch:'full'}
 ];
