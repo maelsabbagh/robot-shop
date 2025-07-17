e8a8298e33af12d6103a698a0b3f7ad486f522a9
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-07-17 12:29:01 +0300

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Cart } from './cart/cart';
+import { Catalog } from './catalog/catalog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:Home,title:'Home - Robot Shop'},
+    {path:'catalog',component:Catalog},
+    {path:'cart',component:Cart}
+];
